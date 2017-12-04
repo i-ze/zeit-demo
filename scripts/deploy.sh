3065c995e5d8c98894f4c77454f662cfdb497778
ze <szopory.zoltan@ustream.tv>
2017-12-04 13:54:11 +0100

    deploy test 7

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #curl -v 'https://zeit-demo-roltrndhmb.now.sh'
-now i-ze/zeit-demo --publish -e foobar="this is it"
+now i-ze/zeit-demo --publish -e foobar="this is it" --token $NOW_GITHUB_DEPLOY_TOKEN
