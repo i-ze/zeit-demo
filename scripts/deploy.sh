facfc56e574dcb26de626a47d19550ee0907af74
ze <szopory.zoltan@ustream.tv>
2017-12-04 18:40:00 +0100

    debug deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#curl -v 'https://zeit-demo-roltrndhmb.now.sh'
-now i-ze/zeit-demo --publish -e foobar="new env var" --token $NOW_GITHUB_DEPLOY_TOKEN
+now i-ze/zeit-demo --publish -e foobar="new env var" --token $NOW_GITHUB_DEPLOY_TOKEN -d
