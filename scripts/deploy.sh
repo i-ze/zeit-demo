3b01e5152e9d8f095e7b9b7842510178cc56f5a5
ze <szopory.zoltan@ustream.tv>
2017-12-04 11:10:53 +0100

    deploy test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -v 'https://zeit-demo-roltrndhmb.now.sh'
+#curl -v 'https://zeit-demo-roltrndhmb.now.sh'
+now i-ze/zeit-demo --publish -e foobar="this is it"
--- src/app.js
+++ src/app.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     console.log("proc.env.foobar", process.env.foobar);
-    res.end('Hello World!  env var set:' + (typeof process.env.foobar != "undefined"));
+    res.end('Hello World!  env var "foobar":' + process.env.foobar);
 }).listen(8080);
