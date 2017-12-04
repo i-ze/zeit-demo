8555064290fcdf9a40df7e9c3257a492afac8201
ze <szopory.zoltan@ustream.tv>
2017-12-04 18:56:37 +0100

    deploy fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-now i-ze/zeit-demo --publish -e foobar="new env var" --token $NOW_GITHUB_DEPLOY_TOKEN -d
+now i-ze/zeit-demo --public -e foobar="new env var" --token $NOW_GITHUB_DEPLOY_TOKEN -d
