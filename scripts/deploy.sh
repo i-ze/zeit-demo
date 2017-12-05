6d4e426bdd6faf55770ce85f0b9e4945ab1fb3e4
ze <szopory.zoltan@ustream.tv>
2017-12-05 02:06:47 +0100

    alias test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 now rm -y zeit-demo --token $NOW_GITHUB_DEPLOY_TOKEN --debug --safe
-now i-ze/zeit-demo --public -e foobar="alma" --token $NOW_GITHUB_DEPLOY_TOKEN --debug
+now i-ze/zeit-demo --public -e foobar="foo" --token $NOW_GITHUB_DEPLOY_TOKEN --debug
