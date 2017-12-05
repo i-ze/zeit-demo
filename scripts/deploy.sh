85a0f022990ef0e53c7c8d1a8364c486c1548567
ze <szopory.zoltan@ustream.tv>
2017-12-05 02:26:34 +0100

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-now i-ze/zeit-demo --public -e foobar="foo" --token $NOW_GITHUB_DEPLOY_TOKEN --debug
+now i-ze/zeit-demo --public -e foobar="it's working" --token $NOW_GITHUB_DEPLOY_TOKEN --debug
 now ln --token $NOW_GITHUB_DEPLOY_TOKEN --debug
 now rm -y zeit-demo --token $NOW_GITHUB_DEPLOY_TOKEN --debug --safe
